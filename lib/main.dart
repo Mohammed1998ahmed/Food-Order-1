acd801f48a064a64b98c136b307d3626c1f05c47
mohammedahmeddd31234 <mohammedahmeddd31234@gmail.com>
2025-09-13 13:59:52 +0300

    -Added importing ✅.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/core/routers/app_router.dart';
 
 import 'app_app.dart';
+import 'core/routers/app_router.dart';
 
 void main() {
   runApp(AppApp(
